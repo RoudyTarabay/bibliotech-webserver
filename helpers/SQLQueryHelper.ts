eca22951e0f9c2e841181c6683d48f93f3fcbe9e
Roudy Tarabay <roudytarabay@gmail.com>
2020-05-30 02:34:42 +0300

    New: SQL Helper which handles mysql calls

---
+++ helpers/SQLQueryHelper.ts
@@ -0,0 +1,21 @@
+import pool from "../database";
+export default class SQLQueryHelper {
+  query: string;
+  queryArgs: [];
+  setQuery(query: string) {
+    this.query = query;
+    return this;
+  }
+  setQueryArgs(args: []) {
+    this.queryArgs = args;
+    return this;
+  }
+  async execute() {
+    try {
+      const response = await pool.query(this.query, this.queryArgs);
+      console.log("executing", response);
+    } catch (err) {
+      console.log("error", err, err.message);
+    }
+  }
+}
